d4817eaaa94fd3f25bc365c793366f7c1dd141ad
Léo Bernard <leodevian.gh@gmail.com>
2025-07-21 22:04:59 +0200

    refactor: add Git user information to globals during generation

--- extensions/git.py
+++ extensions/git.py
@@ -11,26 +11,32 @@ if TYPE_CHECKING:
     from jinja2.environment import Environment
 
 
-def git_user_name(user_name: str) -> str:
-    """Return the Git username."""
-    return (
-        subprocess.check_output(
+def git_user_name() -> str:
+    """Return the username from Git user information."""
+    try:
+        user_name = subprocess.check_output(
             ("git", "config", "user.name"),
             encoding="utf-8",
         ).rstrip()
-        or user_name
-    )
+
+    except subprocess.CalledProcessError:
+        user_name = ""
+
+    return user_name
 
 
-def git_user_email(user_email: str) -> str:
-    """Return the Git user email."""
-    return (
-        subprocess.check_output(
+def git_user_email() -> str:
+    """Return the email address from Git user information."""
+    try:
+        user_email = subprocess.check_output(
             ("git", "config", "user.email"),
             encoding="utf-8",
         ).rstrip()
-        or user_email
-    )
+
+    except subprocess.CalledProcessError:
+        user_email = ""
+
+    return user_email
 
 
 class GitExtension(Extension):
@@ -39,5 +45,5 @@ class GitExtension(Extension):
     def __init__(self, environment: Environment) -> None:
         """Initialize the extension."""
         super().__init__(environment)
-        environment.filters["git_user_name"] = git_user_name
-        environment.filters["git_user_email"] = git_user_email
+        environment.globals["git_user_name"] = git_user_name()
+        environment.globals["git_user_email"] = git_user_email()
