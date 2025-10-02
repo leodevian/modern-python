e925f426ae0b40d33791a4c9ba275b2f158dc622
Léo Bernard <leodevian.gh@gmail.com>
2025-10-03 01:50:30 +0200

    chore: update the release script

--- scripts/release.py
+++ scripts/release.py
@@ -1,27 +1,26 @@
 # /// script
 # requires-python = ">=3.10"
 # dependencies = [
-#   "packaging>=24.2",
 #   "towncrier>=24.8",
-#   "uv>=0.7.7",
+#   "uv>=0.8.6",
 # ]
 # ///
-"""Automation for releases."""
+"""Make a release."""
 
-# ruff: noqa: T201
+# ruff: noqa: S603, T201
 
 from __future__ import annotations
 
 import argparse
+import json
 import subprocess
 import sys
+from contextlib import contextmanager
 from functools import partial
 from typing import TYPE_CHECKING
 
-from packaging.version import Version
-
 if TYPE_CHECKING:
-    from collections.abc import Sequence
+    from collections.abc import Generator, Sequence
 
 print_error = partial(print, file=sys.stderr)
 
@@ -32,133 +31,154 @@ run = partial(
 )
 
 
-def main(argv: Sequence[str] | None = None) -> int:  # noqa: PLR0911, PLR0915
-    """Prepare a new release."""
-    # Parse command-line arguments.
-    parser = argparse.ArgumentParser(description="prepare a new release")
+def create_parser() -> argparse.ArgumentParser:
+    """Create the argument parser."""
+    parser = argparse.ArgumentParser(description="make a release")
     parser.add_argument(
-        "version",
-        type=Version,
-        help="provide the release version",
+        "--bump",
+        required=True,
+        help="update the version using the given semantics",
     )
     parser.add_argument(
         "--dry-run",
         action="store_true",
-        help="perform a dry run",
+        help="do not push changes and reset the repository",
     )
-    args = parser.parse_args(argv)
+    return parser
 
-    # Get the public portion of the version.
-    version = args.version.public
 
-    # Check if the Git repository is dirty.
+def check_repository() -> None:
+    """Check whether the repository is dirty."""
     try:
         run(("git", "diff", "--quiet"))
 
     except subprocess.CalledProcessError:
         print_error("The Git repository is dirty.")
-        return 1
+        raise SystemExit(1) from None
 
-    # Get the name of the base branch.
-    base_branch = subprocess.check_output(
-        ("git", "rev-parse", "--abbrev-ref", "HEAD"),
-        encoding="utf-8",
-    ).rstrip()
 
-    # Create the release branch.
-    release_branch = f"release/{version}"
-
+def bump_version(bump: str) -> str:
+    """Bump the version and return the new version."""
     try:
-        run(("git", "branch", release_branch))
+        version_json = subprocess.check_output(
+            (
+                "uv",
+                "version",
+                "--no-sync",
+                "--output-format",
+                "json",
+                "--bump",
+                bump,
+            )
+        )
 
     except subprocess.CalledProcessError:
-        print_error(f"The release branch already exists: {release_branch!r}.")
-        return 1
+        print_error("An error occurred while bumping the version.")
+        raise SystemExit(1) from None
 
-    print(f"Created release branch {release_branch!r}.")
+    version = json.loads(version_json)
+    return version["version"]
 
-    # Switch to the release branch.
-    run(("git", "checkout", release_branch))
 
-    print(f"Switched from branch {base_branch!r} to release branch {release_branch!r}.")
+@contextmanager
+def switch_to_branch(branch: str) -> Generator[None]:
+    """Create a new branch and switch to it.
 
-    # Bump the version.
+    It is removed on exit.
+    """
+    base_branch = subprocess.check_output(
+        ("git", "rev-parse", "--abbrev-ref", "HEAD"),
+        text=True,
+    ).rstrip()
+
     try:
-        run(("uv", "version", "--no-sync", version))
+        run(("git", "branch", branch))
+
+    except subprocess.CalledProcessError:
+        print_error(f"The branch already exists: {branch!r}.")
+        raise SystemExit(1) from None
+
+    print(f"Created new branch {branch!r}.")
+
+    try:
+        run(("git", "checkout", branch))
+        print(f"Switched from branch {base_branch!r} to branch {branch!r}.")
+        yield
+
+    finally:
+        run(("git", "checkout", base_branch))
+        run(("git", "branch", "-D", branch))
+        print(f"Removed branch {branch!r} and switched back to {base_branch!r}.")
+
+
+def build_changelog(version: str) -> None:
+    """Build the changelog."""
+    try:
         run(("towncrier", "build", "--yes", "--version", version))
 
     except subprocess.CalledProcessError:
-        run(("git", "checkout", base_branch))
-        run(("git", "branch", "-D", release_branch))
-        print_error("An error occurred while bumping the version.")
-        print_error(
-            f"Removed release branch {release_branch!r} "
-            f"and switched back to {base_branch!r}."
-        )
-        return 1
+        print_error("An error occurred while building the changelog.")
+        raise SystemExit(1) from None
 
-    # Commit changes.
-    run(("git", "add", ":/pyproject.toml", ":/uv.lock"))
-    run(("git", "add", "-A", ":/changelog.d/*", ":/CHANGELOG.md"))
-    run(("git", "commit", "-m", f"chore: prepare release {version}", "--no-verify"))
 
-    print(f"Committed changes on branch {release_branch!r}.")
-
-    # Create the release tag.
+def create_release_tag(version: str) -> str:
+    """Create the release tag."""
     release_tag = f"v{version}"
+    message = f"bump version to {version}"
 
     try:
-        run(("git", "tag", "-a", release_tag, "-m", f"bump version to {version}"))
+        run(("git", "tag", "-a", release_tag, "-m", message))
 
     except subprocess.CalledProcessError:
-        run(("git", "checkout", base_branch))
-        run(("git", "branch", "-D", release_branch))
         print_error(f"The release tag already exists: {release_tag!r}.")
-        print_error(
-            f"Removed release branch {release_branch!r} "
-            f"and switched back to {base_branch!r}."
-        )
-        return 1
+        raise SystemExit(1) from None
 
     print(f"Created release tag {release_tag!r}.")
+    return release_tag
 
-    # Exit on dry run.
-    if args.dry_run:
-        print("Dry run success!")
-        run(("git", "checkout", base_branch))
-        run(("git", "branch", "-D", release_branch))
-        print(
-            f"Removed release branch {release_branch!r} "
-            f"and switched back to {base_branch!r}."
-        )
-        run(("git", "tag", "-d", release_tag))
-        print(f"Removed release tag {release_tag!r}.")
-        return 0
 
-    # Push changes to the remote repository and push the release tag.
-    try:
-        run(("git", "push", "origin", f"{release_branch}:main", "--follow-tags"))
+def push_changes(branch: str, remote_branch: str = "main") -> None:
+    """Push the changes to the target branch."""
+    run(("git", "push", "origin", f"{branch}:{remote_branch}", "--follow-tags"))
+    print(f"Pushed changes from {branch!r} to 'origin/{remote_branch}'.")
 
-    except subprocess.CalledProcessError:
-        run(("git", "checkout", base_branch))
-        run(("git", "branch", "-D", release_branch))
-        print_error("An error occurred while pushing changes.")
-        print_error(
-            f"Removed release branch {release_branch!r} "
-            f"and switched back to {base_branch!r}."
-        )
-        run(("git", "tag", "-d", release_tag))
-        print_error(f"Removed release tag {release_tag!r}.")
-        return 1
 
-    print(f"Pushed changes from {release_branch!r} to 'origin/main'.")
+def main(argv: Sequence[str] | None = None) -> None:
+    """Prepare a new release."""
+    parser = create_parser()
+    args = parser.parse_args(argv)
 
-    # Remove the release branch and switch to the main branch.
+    check_repository()
+
+    version = bump_version(args.bump)
+
+    release_branch = f"release/{version}"
+
+    with switch_to_branch(release_branch):
+        build_changelog(version)
+
+        # Commit changes.
+        run(("git", "add", ":/pyproject.toml", ":/uv.lock"))
+        run(("git", "add", "-A", ":/changelog.d/*", ":/CHANGELOG.md"))
+        message = f"chore: prepare release {version}"
+        run(("git", "commit", "--no-verify", "-m", message))
+        print(f"Committed changes on branch {release_branch!r}.")
+
+        release_tag = create_release_tag(version)
+
+        # Exit on dry run.
+        if args.dry_run:
+            print("Dry run success!")
+            run(("git", "tag", "-d", release_tag))
+            print(f"Removed release tag {release_tag!r}.")
+            return
+
+        push_changes(release_branch, "main")
+
+    # Switch to the main branch.
     run(("git", "checkout", "main"))
-    run(("git", "branch", "-D", release_branch))
     run(("git", "fetch"))
     run(("git", "reset", "--hard", "origin/main"))
-    return 0
 
 
 if __name__ == "__main__":
